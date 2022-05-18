4a604fa4d2a89098bfdc9150e25b518b7097f434
Witoon Wiphusitphunpol <tenshi623@gmail.com>
2022-05-19 02:15:05 +0700

    return tensor on the same device as the model

--- sac/network.py
+++ sac/network.py
@@ -38,7 +38,7 @@ class StateEncoderWrapper(Container):
     def encode(self, observation, return_tensor=False):
         if isinstance(self.encoder, VAEBase):
             observation = np.expand_dims(observation, axis=0)
-            encoded = encode_vae_observation(observation, self.encoder, device=self.device, normalize=False)
+            encoded = encode_vae_observation(observation, self.encoder, device=self.device, normalize=False, output_device=self.device)
         elif isinstance(observation, np.ndarray):
             obs_dtype = torch.float16 if observation.dtype == np.float16 else torch.float32
             observation = torch.tensor(observation, dtype=obs_dtype, device=self.device).unsqueeze(dim=0)
